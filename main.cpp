14f97e3933c4c2d765a88d7925ec95d4a6d20a3d
ling <yslingshu@163.com>
2021-01-05 15:54:49 +0800

    创建main.cpp文件

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{
+	
+	
+}
+
+int main()
+{
+	int arr[]={12,3,56,46,42,23};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+	
+}
